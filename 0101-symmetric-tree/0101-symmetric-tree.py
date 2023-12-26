d88ca7e150227472511bf232468e613e70209c21
Jeremy Park <107975543+Jeremy-0204@users.noreply.github.com>
2023-12-27 07:25:23 +0900

    Time: 41 ms (56.31%), Space: 17.5 MB (5.47%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root: 
+            return True
+        
+        stack = []
+        stack.append(root.left)
+        stack.append(root.right)
+        
+        while stack:
+            node1 = stack.pop()
+            node2 = stack.pop()
+            
+            if not node1 and not node2:
+                continue
+            
+            if not node1 or not node2:
+                return False
+            
+            if node1.val != node2.val:
+                return False
+            
+            stack.append(node1.left)
+            stack.append(node2.right)
+            stack.append(node1.right)
+            stack.append(node2.left)
+        
+        return True
+            
