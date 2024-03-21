cb38afba1c52cf5ba8e0e726b77fd09b5f9fb7e0
Jeremy Park <107975543+Jeremy-0204@users.noreply.github.com>
2024-03-21 21:09:04 +0900

    Time: 38 ms (36.47%) | Memory: 16.6 MB (21.24%) - LeetSync

---
+++ 504-base-7/base-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        ans = ""
+        temp = num
+        num = abs(num)
+
+        while num > 0:
+            ans += str(num % 7)
+            num //= 7
+        
+        neg = "-"+ans[::-1]
+        return ans[::-1] if temp > 0 else "0" if temp == 0 else neg
